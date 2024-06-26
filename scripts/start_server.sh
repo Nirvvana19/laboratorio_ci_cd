8f585e899f4fccb983cbfa99b3b6aa28afa4e58c
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-06-26 16:46:06 -0500

    Actualizacion

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Variables
+REGION="us-west-2"
+ACCOUNT_ID="338287058401"
+REPOSITORY_NAME="laboratorio_mafe"
+IMAGE_TAG="latest"
+
 # Autenticarse en ECR
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 338287058401.dkr.ecr.us-east-1.amazonaws.com
+$(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
 
 # Extraer la última imagen de Docker desde ECR
-docker pull 338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG
 
 # Ejecutar la aplicación
-docker run -d -p 80:80 --name labo_mafe_aws 338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest
+docker run -d -p 80:80 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG
