b6055fe93be1604002da6aa8e11d1f648569fed9
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-06-26 14:31:09 -0500

    Actualizacion

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Aplicar los archivos de configuración de Kubernetes
-kubectl apply -f /home/ec2-user/laboratorio_ci_cd/deployment.yml
-kubectl apply -f /home/ec2-user/laboratorio_ci_cd/service.yml
---
+++ scripts/start_server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -p 80:80 --name  labo_mafe_aws 338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe
