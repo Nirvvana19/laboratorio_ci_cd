30f8df47d1d86b5cfe3fc3b021dafa46a83f0316
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-06-26 16:40:01 -0500

    Actualizacion

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Autenticarse en ECR
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 338287058401.dkr.ecr.us-east-1.amazonaws.com
+
+# Extraer la última imagen de Docker desde ECR
+docker pull 338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest
+
+# Ejecutar la aplicación
 docker run -d -p 80:80 --name labo_mafe_aws 338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest
