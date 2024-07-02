2b9fc4a154af5a28ad1a32a9bbfad8a8b4196ffc
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-07-02 18:19:35 -0500

    Actualizacion

--- scripts/deploy_to_minikube.sh
+++ scripts/deploy_to_minikube.sh
@@ -11,5 +11,8 @@ kubectl config use-context minikube
 # Asegúrate de que el namespace exista
 kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
 
+# Autenticarse en ECR
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
 # Actualiza el deployment en Minikube con la nueva imagen
 kubectl set image deployment/laboratorio-mafe -n $NAMESPACE laboratorio-mafe=338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest
