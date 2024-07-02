d63137b183bc7e19000d475db337c90e6d20d3ae
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-07-02 16:50:18 -0500

    Actualizacion

---
+++ scripts/deploy_to_minikube.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Nombre del namespace
+NAMESPACE=lab-mafe-ci-cd
+
+# Configura el contexto de Kubernetes para Minikube
+kubectl config set-cluster minikube --server=https://192.168.49.2:8443 --insecure-skip-tls-verify
+kubectl config set-context minikube --cluster=minikube --namespace=$NAMESPACE
+kubectl config use-context minikube
+
+# Asegúrate de que el namespace exista
+kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
+
+# Actualiza el deployment en Minikube con la nueva imagen
+kubectl set image deployment/laboratorio-mafe -n $NAMESPACE laboratorio-mafe=338287058401.dkr.ecr.us-west-2.amazonaws.com/laboratorio_mafe:latest --record
