a739aae44fc9ecd67bf2519bdd24e2b612a25367
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-07-01 23:16:30 -0500

    Actualizacion

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,25 +5,25 @@ REGION="us-west-2"
 ACCOUNT_ID="338287058401"
 REPOSITORY_NAME="laboratorio_mafe"
 IMAGE_TAG="latest"
+NAMESPACE="lab-mafe-ci-cd"
 
 # Configura el contexto de Kubernetes para apuntar a Minikube
 kubectl config use-context minikube
 
-# Iniciar Minikube
- minikube start
+# Iniciar Minikube con el controlador none y cri-dockerd
+minikube start --driver=none --container-runtime=cri-dockerd
 
 # Autenticarse en ECR
-$(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 # Extraer la última imagen de Docker desde ECR
 docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG
 
 # Aplicar el despliegue a Minikube
- kubectl apply -f deployment.yaml -n lab-mafe-ci-cd
+kubectl apply -f /home/ec2-user/laboratorio_ci_cd/k8s/deployment.yaml -n $NAMESPACE
 
-# Aplicar el despliegue a Minikube
- kubectl apply -f service.yaml -n lab-mafe-ci-cd
+# Aplicar el servicio a Minikube
+kubectl apply -f /home/ec2-user/laboratorio_ci_cd/k8s/service.yaml -n $NAMESPACE
 
 # Obtener la URL del servicio
- minikube service laboratorio-mafe-service 
-
+minikube service laboratorio-mafe-service -n $NAMESPACE
