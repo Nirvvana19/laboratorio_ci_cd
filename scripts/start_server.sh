8382fc9928f3bfba13ddb85aa7b78deb67959385
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-07-01 22:37:14 -0500

    Actualizacion

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,12 +6,11 @@ ACCOUNT_ID="338287058401"
 REPOSITORY_NAME="laboratorio_mafe"
 IMAGE_TAG="latest"
 
+# Configura el contexto de Kubernetes para apuntar a Minikube
+kubectl config use-context minikube
 
 # Iniciar Minikube
-# minikube start
-
-# Iniciar Docker dentro de Minikube
-# eval $(minikube docker-env)
+ minikube start
 
 # Autenticarse en ECR
 $(aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com)
@@ -19,16 +18,12 @@ $(aws ecr get-login-password --region $REGION | docker login --username AWS --pa
 # Extraer la última imagen de Docker desde ECR
 docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG
 
-
-# Cargar la imagen en Minikube
-# minikube image load $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY:$IMAGE_TAG
+# Aplicar el despliegue a Minikube
+ kubectl apply -f deployment.yaml -n lab-mafe-ci-cd
 
 # Aplicar el despliegue a Minikube
-# kubectl apply -f $DEPLOYMENT_FILE
-
-# Aplicar el despliegue a Minikube
-# kubectl apply -f $SERVICE_FILE
+ kubectl apply -f service.yaml -n lab-mafe-ci-cd
 
 # Obtener la URL del servicio
-# minikube service laboratorio-mafe-service 
+ minikube service laboratorio-mafe-service 
 
