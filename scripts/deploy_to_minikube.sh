e65446d52097f116623d10e5213de4871cd80692
Nirvvana19 <quinterosmariafernanda352@gmail.com>
2024-07-04 18:59:39 -0500

    Actualizacion

--- scripts/deploy_to_minikube.sh
+++ scripts/deploy_to_minikube.sh
@@ -7,15 +7,12 @@ REPOSITORY_NAME="laboratorio_mafe"
 IMAGE_TAG="latest"
 NAMESPACE="lab-mafe-ci-cd"
 
-echo "Hola"
+echo "Casa"
 export KUBECONFIG=/home/maria-fernanda/.kube/config
 
-kubectl get ns
-
-
-# Asegúrate de que el namespace exista en Minikube
-echo "Verificando namespace en Minikube..."
-if kubectl get namespace $NAMESPACE &> /dev/null; then
+# Asegúrate de que el namespace exista
+echo "Verificando namespace..."
+if kubectl get namespace $NAMESPACE; then
   echo "Namespace $NAMESPACE encontrado."
 else
   echo "Namespace $NAMESPACE no encontrado. Creando namespace..."
@@ -28,29 +25,37 @@ else
 fi
 
 # Autenticación en ECR y actualización del deployment en Minikube
-echo "Iniciando sesión en Docker..."
+echo "Logging into Docker..."
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 if [ $? -ne 0 ]; then
-  echo "Error al iniciar sesión en ECR"
+  echo "Failed to login to ECR"
   exit 1
 fi
 
-# Actualizar el deployment en Minikube con la nueva imagen y la URL de Ngrok
-echo "Actualizando imagen y URL en Minikube..."
+# Actualizar el deployment en Minikube con la nueva imagen
+echo "Actualizando imagen..."
 kubectl set image deployment/laboratorio-mafe -n $NAMESPACE laboratorio-mafe=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG
 
-# Actualizar el servicio para usar la URL de Ngrok
-sed -i "s|NGROK_URL_PLACEHOLDER|$NGROK_URL|g" ./service.yaml
-kubectl apply -f ./service.yaml -n $NAMESPACE
+if [ $? -ne 0 ]; then
+  echo "Failed to set image"
+  exit 1
+fi
 
 # Aplicar los archivos de configuración de Kubernetes
 echo "Aplicando configuración de Kubernetes..."
 kubectl apply -f ./deployment.yaml -n $NAMESPACE
 
 if [ $? -ne 0 ]; then
-  echo "Error al aplicar la configuración de deployment"
+  echo "Failed to apply deployment configuration"
   exit 1
 fi
 
-echo "Despliegue actualizado y configuración aplicada con éxito."
+kubectl apply -f ./service.yaml -n $NAMESPACE
+
+if [ $? -ne 0 ]; then
+  echo "Failed to apply service configuration"
+  exit 1
+fi
+
+echo "Deployment actualizado y configuración aplicada con éxito."
